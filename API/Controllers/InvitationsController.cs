e834d35ebc99bea0f45d72cc4c06a8ef7fb16cbc
giladaharoni <giladah1@gmail.com>
2022-05-30 11:30:56 +0300

    G

---
+++ API/Controllers/InvitationsController.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvitationsController : ControllerBase
+    {
+        private static List<Invitation> _invitations = new List<Invitation>();
+
+
+       [HttpPost]
+        public void post(Invitation invitation)
+        {
+            _invitations.Add(invitation);
+
+        }
+    }
+}
---
+++ API/Controllers/transfersController.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/transfer")]
+    [ApiController]
+    public class transfersController : ControllerBase
+    {
+        private static List<transfer> _transfers = new List<transfer>();
+
+
+        [HttpPost]
+        public void post(transfer transfer)
+        {
+            _transfers.Add(transfer);
+
+        }
+
+    }
+}
